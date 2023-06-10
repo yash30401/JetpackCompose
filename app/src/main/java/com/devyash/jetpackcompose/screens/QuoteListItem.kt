a3a498808be94a49bd2247160e37dec1e78ae7fe
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-10 18:04:17 +0530

    QuoteDetail created

--- app/src/main/java/com/devyash/jetpackcompose/screens/QuoteListItem.kt
+++ app/src/main/java/com/devyash/jetpackcompose/screens/QuoteListItem.kt
@@ -2,8 +2,10 @@ package com.devyash.jetpackcompose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,16 +21,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devyash.jetpackcompose.R
 import com.devyash.jetpackcompose.ui.theme.Grey
 
 
-@Preview
 @Composable
 fun QuoteListItem() {
     Card(
@@ -52,14 +57,64 @@ fun QuoteListItem() {
                     modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp),
                     fontWeight = FontWeight.SemiBold
                 )
-            Box(modifier = Modifier
-                .background(Grey)
-                .fillMaxWidth(.8f)
-                .height(1.dp)) {
-            }
+                Box(
+                    modifier = Modifier
+                        .background(Grey)
+                        .fillMaxWidth(.8f)
+                        .height(1.dp)
+                ) {
+                }
                 Text(text = "Theofrastus", Modifier.padding(4.dp), fontWeight = FontWeight.Thin)
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(4.dp),
+            modifier = Modifier.padding(32.dp)
+        ) {
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_format_quote_24),
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .rotate(180f)
+                        .size(80.dp)
+                )
+                Text(
+                    text = "Time is the most valuable thing a man can spend",
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_medium_italic))
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Theofrastus", Modifier.padding(4.dp), fontWeight = FontWeight.Thin)
+            }
+
+        }
+
     }
 }
 
